49fe239f397034937c6310e239648acb329b3c17
Cayo M Amorim <cayomesquita@gmail.com>
2020-08-31 23:10:37 -0700

    Complete Lab OOP

--- HelloKotlin/src/example/myapp/AquariumFish.kt
+++ HelloKotlin/src/example/myapp/AquariumFish.kt
@@ -1,4 +1,41 @@
 package example.myapp
 
-class AquariumFish {
+class Shark: FishColor, FishAction {
+    override val color = "gray"
+    override fun eat() {
+        println("hunt and eat fish")
+    }
+}
+
+class Plecostomus (fishColor: FishColor = GoldColor):
+    FishAction by PrintingFishAction("eat algae"),
+    FishColor by fishColor
+
+object GoldColor : FishColor {
+    override val color = "gold"
+}
+
+class PrintingFishAction(val food: String) : FishAction {
+    override fun eat() {
+        println(food)
+    }
+}
+
+interface FishAction  {
+    fun eat()
+}
+
+interface FishColor {
+    val color: String
+}
+
+sealed class Seal
+class SeaLion : Seal()
+class Walrus : Seal()
+
+fun matchSeal(seal: Seal): String {
+    return when(seal) {
+        is Walrus -> "walrus"
+        is SeaLion -> "sea lion"
+    }
 }
